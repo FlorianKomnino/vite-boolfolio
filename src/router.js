0c53cd76c8fe0e4f0719882aee79d517532f2b21
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 15:57:01 +0100

    created first routes in router.js

--- src/router.js
+++ src/router.js
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import MainComponent from './pages/MainComponent.vue';
+import HomePage from './pages/HomePage.vue';
 import ProjectsIndex from './pages/ProjectsIndex.vue';
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/index',
+            name: 'index',
+            component: ProjectsIndex
+        }
+    ]
+});
 
 export { router };
