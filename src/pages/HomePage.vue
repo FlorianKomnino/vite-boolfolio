d23e00907af0c2ebd34a834fde222f234a119c54
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 15:58:34 +0100

    populated basic pages/HomePage.vue

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+    name: 'Home',
+    components: {
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>
+                    Home Page
+                </h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
