70989fcb39c323a9ed54a876360530f6305f7c50
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 16:33:24 +0100

    implemented ternary for active class in navbar links

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -16,17 +16,17 @@ export default {
             </a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">
+                    <router-link :to="{ name: 'home' }" class="nav-link" :class="this.$route.name === 'home' ? 'active': ''" aria-current="page">
                         Home
                     </router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link :to="{ name: 'index' }" class="nav-link active">
+                    <router-link :to="{ name: 'index' }" class="nav-link" :class="this.$route.name === 'index' ? 'active': ''" aria-current="page">
                         Projects List
                     </router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link :to="{ name: 'aboutUs' }" class="nav-link active">
+                    <router-link :to="{ name: 'aboutUs' }" class="nav-link" :class="this.$route.name === 'aboutUs' ? 'active': ''" aria-current="page">
                         About Us
                     </router-link>
                 </li>
