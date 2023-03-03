1e79806f0dd83fe8be8e9eab7a8736a0eecf17ca
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 15:33:36 +0100

    having a problem with vue-router

---
+++ src/router.js
@@ -0,0 +1,7 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import MainComponent from './pages/MainComponent.vue';
+import ProjectsIndex from './pages/ProjectsIndex.vue';
+
+
+export { router };
