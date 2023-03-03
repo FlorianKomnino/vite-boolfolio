b899c406e9ab470f104cc986968f24606568546d
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 15:58:02 +0100

    added navbar to HeaderComponent.vue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,9 +9,21 @@ export default {
 </script>
 
 <template lang="">
-    <div>
-        
-    </div>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Navbar
+            </a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <style lang="scss" scoped></style>
