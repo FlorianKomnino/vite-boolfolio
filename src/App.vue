8e94632c75c8c000aa7d07396b116d31f6598147
FlorianKomnino <florian.komnino@outlook.it>
2023-03-03 15:19:11 +0100

    added header component and router-view tag to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script>
 import MainComponent from './components/MainComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
 
 export default {
   name: 'App',
   components: {
+    HeaderComponent,
     MainComponent
   }
 }
 </script>
 
 <template>
+  <header>
+    <HeaderComponent />
+  </header>
+
   <main>
     <MainComponent />
+    <router-view></router-view>
   </main>
 </template>
 
